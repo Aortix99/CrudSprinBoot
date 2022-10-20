cd016619c413000412f6d03b11c0e5757a16dc91
Aortix99 <acocogollo@gmail.com>
2022-10-20 08:01:16 -0500

    first commit

---
+++ src/test/java/com/fundamentosplazi/fndamentos/FndamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplazi.fndamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FndamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
